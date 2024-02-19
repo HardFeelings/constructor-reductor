718a64604cc4c6e647fee33ce42e8b860a9b2c18
HardFeelings <shatsckij.i@yandex.ru>
2024-02-19 06:23:03 +0300

    add db migration

--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/controller/ReducerTypeController.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/controller/ReducerTypeController.java
@@ -6,19 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.vpt.constructorapp.api.reducer.type.ReducerTypeApi;
 import ru.vpt.constructorapp.api.reducer.type.dto.ReducerTypeDto;
 import ru.vpt.constructorapp.api.util.ResponseDto;
+import ru.vpt.constructorapp.service.ReducerTypeService;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ReducerTypeController extends AbstractResponseController implements ReducerTypeApi {
+    private final ReducerTypeService reducerTypeService;
     @Override
     public ResponseEntity<ResponseDto<List<ReducerTypeDto>>> getAllReducerTypes() {
-        return null;
+        return response(reducerTypeService.getAllReducerTypes());
     }
 
     @Override
     public ResponseEntity<ResponseDto<ReducerTypeDto>> getById(Long id) {
-        return null;
+        return response(reducerTypeService.getById(id));
     }
 }
