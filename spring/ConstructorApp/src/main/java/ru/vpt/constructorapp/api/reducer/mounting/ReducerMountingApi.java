2930c6c407dcee0db0f8dd4d99c703fd2ef301b7
Dmitry Serebryansky <d.srb@yandex.ru>
2024-05-15 21:53:14 +0300

    Создание методов сохранения и удаления для ReducerMounting

--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/api/reducer/mounting/ReducerMountingApi.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/api/reducer/mounting/ReducerMountingApi.java
@@ -1,19 +1,23 @@
 package ru.vpt.constructorapp.api.reducer.mounting;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.vpt.constructorapp.api.reducer.mounting.dto.ReducerMountingDto;
 import ru.vpt.constructorapp.api.util.ResponseDto;
 
 import java.util.List;
 
-@RequestMapping("api/v1/reducerMounting")
+@RequestMapping("/security/reducerMounting")
 public interface ReducerMountingApi {
     @GetMapping
     ResponseEntity<ResponseDto<List<ReducerMountingDto>>> getAllReducerMounting();
 
     @GetMapping("/{id}")
     ResponseEntity<ResponseDto<ReducerMountingDto>> getById(@PathVariable("id") Long id);
+
+    @PostMapping
+    ResponseEntity<ResponseDto<ReducerMountingDto>> save(@RequestBody ReducerMountingDto reducerMountingDto);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<ResponseDto<Boolean>> delete(@PathVariable("id") Long id);
 }
--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/reducer/ReducerMountingService.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/reducer/ReducerMountingService.java
@@ -1,10 +1,14 @@
 package ru.vpt.constructorapp.service.reducer;
 
 import ru.vpt.constructorapp.api.reducer.mounting.dto.ReducerMountingDto;
+import ru.vpt.constructorapp.store.entities.reducer.ReducerMountingEntity;
 
 import java.util.List;
 
 public interface ReducerMountingService {
     List<ReducerMountingDto> getAllReducerMounting();
     ReducerMountingDto getReducerMountingById(Long id);
+    ReducerMountingDto saveReducerMounting(ReducerMountingDto reducerMountingDto);
+    Boolean deleteReducerMounting(Long id);
+    ReducerMountingEntity findById(Long id);
 }
