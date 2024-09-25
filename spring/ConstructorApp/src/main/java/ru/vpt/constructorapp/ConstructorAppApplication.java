0cde46084189174a64e9be23722dfb36738c6216
HardFeelings <shatsckij.i@yandex.ru>
2024-09-25 14:51:50 +0300

    nginx

--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/ConstructorAppApplication.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/ConstructorAppApplication.java
@@ -2,8 +2,6 @@ package ru.vpt.constructorapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class ConstructorAppApplication {
