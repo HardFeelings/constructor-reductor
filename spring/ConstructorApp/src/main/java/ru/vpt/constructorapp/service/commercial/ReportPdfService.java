4745fea8d88230366629250b2e86520532739fcc
HardFeelings <shatsckij.i@yandex.ru>
2024-10-14 00:04:46 +0300

    fix pdf report

--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/commercial/ReportPdfService.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/commercial/ReportPdfService.java
@@ -1,8 +1,4 @@
 package ru.vpt.constructorapp.service.commercial;
-import com.aspose.cells.PdfSaveOptions;
-import com.aspose.cells.SaveFormat;
-import com.aspose.cells.SaveOptions;
-import com.aspose.cells.Workbook;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.vpt.constructorapp.store.entities.commercial.CommercialPropEntity;
@@ -17,13 +13,6 @@ public class ReportPdfService {
     public InputStream report(CommercialPropEntity byId) {
         InputStream xls = reportService.report(byId);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Workbook workbook = null;
-        try {
-            workbook = new Workbook(xls);
-            workbook.save(baos, SaveFormat.PDF);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
         return new ByteArrayInputStream(baos.toByteArray());
     }
 }
