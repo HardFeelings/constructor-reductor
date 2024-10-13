8fb3d024a3ca3abc4e73f26b3fc77c5829705139
HardFeelings <shatsckij.i@yandex.ru>
2024-10-13 19:31:45 +0300

    fix pdf report

--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/commercial/ReportPdfService.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/commercial/ReportPdfService.java
@@ -1,13 +1,12 @@
 package ru.vpt.constructorapp.service.commercial;
 
-import com.aspose.cloud.cells.api.CellsApi;
+import com.spire.xls.FileFormat;
+import com.spire.xls.Workbook;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 import ru.vpt.constructorapp.store.entities.commercial.CommercialPropEntity;
 
 import java.io.*;
-import java.util.Date;
 
 
 @Service
@@ -17,32 +16,13 @@ public class ReportPdfService {
     private final ReportService reportService;
 
     public InputStream report(CommercialPropEntity byId)  {
-        try
-        {
-            InputStream xls = reportService.report(byId);
-            String clientId = "304c1112-e3b3-453c-bd7b-59f74abb6c88";
-            String clientSecret = "4b83766e02e54eb4c976b7b315f42dff";
-            CellsApi api = new CellsApi(clientId,clientSecret);
-
-            String inputName = "targetFile-" + byId.getNumber() + ".xlsx";
-            String outputName = "report-" + byId.getNumber() + ".pdf";
-            String password = null;
-            Boolean isAutoFit = false;
-            Boolean onlySaveTable = false;
-            String format = "PDF";
-            File targetFile = new File(inputName);
-            FileUtils.copyInputStreamToFile(xls, targetFile);
-            api.uploadFile(inputName, targetFile, "constructor");
-            api.cellsWorkbookGetWorkbook(inputName,  password,format,
-                    isAutoFit, onlySaveTable, null,"constructor",  outputName,"constructor", null);
-            File response = api.downloadFile(outputName,"constructor", null );
-            System.out.println("XlSX sucessfully converted to PDF format !");
-            return new FileInputStream(response);
-        }catch(Exception ex)
-        {
-            System.out.println(ex);
-        }
-        return null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        InputStream xls = reportService.report(byId);
+        Workbook workbook = new Workbook();
+        workbook.loadFromStream(xls);
+        workbook.getConverterSetting().setSheetFitToPage(true);
+        workbook.saveToStream(baos, FileFormat.PDF);
+        return new ByteArrayInputStream(baos.toByteArray());
     }
 }
 
