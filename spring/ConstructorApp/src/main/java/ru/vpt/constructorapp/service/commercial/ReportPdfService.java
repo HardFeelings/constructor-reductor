edc7ecd9b50d380241769ffa6b0721d2beb6a314
HardFeelings <shatsckij.i@yandex.ru>
2024-10-13 15:16:14 +0300

    fix pdf report

--- spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/commercial/ReportPdfService.java
+++ spring/ConstructorApp/src/main/java/ru/vpt/constructorapp/service/commercial/ReportPdfService.java
@@ -1,14 +1,13 @@
 package ru.vpt.constructorapp.service.commercial;
 
-import com.convertapi.client.Config;
-import com.convertapi.client.ConvertApi;
-import com.convertapi.client.Param;
+import com.aspose.cloud.cells.api.CellsApi;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 import ru.vpt.constructorapp.store.entities.commercial.CommercialPropEntity;
 
 import java.io.*;
-import java.util.concurrent.ExecutionException;
+import java.util.Date;
 
 
 @Service
@@ -18,17 +17,32 @@ public class ReportPdfService {
     private final ReportService reportService;
 
     public InputStream report(CommercialPropEntity byId)  {
-        InputStream xls = reportService.report(byId);
-        Config.setDefaultSecret("secret_hjxdQyEHipEUFevZ");
-        InputStream output = null;
-        try {
-            output = ConvertApi.convert("xlsx", "pdf",
-                    new Param("File", xls, "reportTemplate.xlsx")
-            ).get().asStream().get();
-        } catch (InterruptedException | ExecutionException e) {
-            System.out.println(e.getMessage());
+        try
+        {
+            InputStream xls = reportService.report(byId);
+            String clientId = "304c1112-e3b3-453c-bd7b-59f74abb6c88";
+            String clientSecret = "4b83766e02e54eb4c976b7b315f42dff";
+            CellsApi api = new CellsApi(clientId,clientSecret);
+
+            String inputName = "targetFile-" + byId.getNumber() + ".xlsx";
+            String outputName = "report-" + byId.getNumber() + ".pdf";
+            String password = null;
+            Boolean isAutoFit = false;
+            Boolean onlySaveTable = false;
+            String format = "PDF";
+            File targetFile = new File(inputName);
+            FileUtils.copyInputStreamToFile(xls, targetFile);
+            api.uploadFile(inputName, targetFile, "constructor");
+            api.cellsWorkbookGetWorkbook(inputName,  password,format,
+                    isAutoFit, onlySaveTable, null,"constructor",  outputName,"constructor", null);
+            File response = api.downloadFile(outputName,"constructor", null );
+            System.out.println("XlSX sucessfully converted to PDF format !");
+            return new FileInputStream(response);
+        }catch(Exception ex)
+        {
+            System.out.println(ex);
         }
-        return output;
+        return null;
     }
 }
 
